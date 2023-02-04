0a065a3c829e7acab19e63865479061c85644ae0
Kai Oda <kaioda802@gmail.com>
2023-02-04 14:28:46 -0800

    format

--- hack-uci/src/scripts/processPosts.py
+++ hack-uci/src/scripts/processPosts.py
@@ -6,7 +6,8 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from src.classes.post import Post
 
-def read_new(): # returns dict of Posts
+
+def read_new():  # returns dict of Posts
     newFile = os.path.join(
         os.path.dirname(__file__), "..", "..", "public", "new_posts.json"
     )
@@ -29,18 +30,18 @@ def read_new(): # returns dict of Posts
         new_posts[new_post.id] = new_post
 
     return new_posts
-    
-def get_old(): # returns dict of Posts
-    newFile = os.path.join(
-        os.path.dirname(__file__), "..", "..", "public", "data.py"
-    )
+
+
+def get_old():  # returns dict of Posts
+    newFile = os.path.join(os.path.dirname(__file__), "..", "..", "public", "data.py")
     new_path = Path(newFile)
-    
+
     with open(new_path, "r") as f:
         adict = f.read()
-    if adict == "": return dict()
+    if adict == "":
+        return dict()
     adict = eval(adict)
-   
+
     old_data = dict()
 
     for id, p in adict.items():
@@ -57,22 +58,30 @@ def get_old(): # returns dict of Posts
 
     return old_data
 
+
 def print_posts(posts: dict[Post]):
     for id, p in posts.items():
         print(str(id) + ": " + p.toString())
 
+
 def write_data(posts: dict[Post]):
-    newFile = os.path.join(
-        os.path.dirname(__file__), "..", "..", "public", "data.json"
-    )
+    newFile = os.path.join(os.path.dirname(__file__), "..", "..", "public", "data.json")
     new_path = Path(newFile)
     new_data = dict()
 
     for id, p in posts.items():
-        new_data[id] = {"date": p.date, "image": p.image, "description": p.description, "tags": p.tags, "authorID": p.authorID, "likes": p.likes, "comments": p.comments}
+        new_data[id] = {
+            "date": p.date,
+            "image": p.image,
+            "description": p.description,
+            "tags": p.tags,
+            "authorID": p.authorID,
+            "likes": p.likes,
+            "comments": p.comments,
+        }
     all_posts = json.dumps(new_data, indent=4)
     # write final changes to data.json
-    with open(new_path, 'w') as storage:
+    with open(new_path, "w") as storage:
         storage.write(all_posts)
 
     secondFile = os.path.join(
@@ -82,19 +91,14 @@ def write_data(posts: dict[Post]):
     # write final changes to data.py
     with open(data, "w") as f:
         f.write(str(new_data))
-    
+
 
 if __name__ == "__main__":
     new_changes = read_new()
     # print_posts(new_changes)
     old = get_old()
-    if old: 
+    if old:
         old.update(new_changes)
-    else: 
+    else:
         old = new_changes
     write_data(old)
-    
-
-    
-
-    
