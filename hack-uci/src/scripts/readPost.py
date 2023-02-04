af0d3c6bf25b4ef1f2c0d20e5f01dbc5f8bf8210
Kai Oda <kaioda802@gmail.com>
2023-02-04 11:46:30 -0800

    change

--- hack-uci/src/scripts/readPost.py
+++ hack-uci/src/scripts/readPost.py
@@ -3,22 +3,30 @@ import json
 import os
 import sys
 
-sys.path.insert(0, '.')
-ROOT_DIR = os.path.abspath(os.curdir)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from src.classes.post import Post
 
 if __name__ == "__main__":
 
-    newFile = ROOT_DIR + "/public/new_posts.json"
+    newFile = os.path.join(
+        os.path.dirname(__file__), "..", "..", "public", "new_posts.json"
+    )
     new_path = Path(newFile)
 
     f = open(new_path)
     data = json.load(f)
     new_posts = dict()
 
-    for p in data['posts']:
-        new_post = Post(p['date'], p['image'], p['description'], p['tags'], p['likes'], p['comments'])
+    for p in data["posts"]:
+        new_post = Post(
+            p["date"],
+            p["image"],
+            p["description"],
+            p["tags"],
+            p["likes"],
+            p["comments"],
+        )
         new_posts[Post.ID] = new_post
 
     for id, p in new_posts.items():
