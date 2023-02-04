dfd9ef576a827b17306e0722e78995603af95483
Byron Fong <byron.fong@gmail.com>
2023-02-04 13:07:08 -0800

    added data.py

--- hack-uci/src/scripts/processPosts.py
+++ hack-uci/src/scripts/processPosts.py
@@ -6,7 +6,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from src.classes.post import Post
 
-def read_post(): # returns dict of Posts
+def read_new(): # returns dict of Posts
     newFile = os.path.join(
         os.path.dirname(__file__), "..", "..", "public", "new_posts.json"
     )
@@ -29,32 +29,64 @@ def read_post(): # returns dict of Posts
 
     return new_posts
     
+def get_old(): # returns dict of Posts
+    newFile = os.path.join(
+        os.path.dirname(__file__), "..", "..", "public", "data.py"
+    )
+    new_path = Path(newFile)
+    
+    with open(new_path, "r") as f:
+        adict = f.read()
+    adict = eval(adict)
+   
+    old_data = dict()
+
+    for id, p in adict.items():
+        post = Post(
+            p["date"],
+            p["image"],
+            p["description"],
+            p["tags"],
+            p["likes"],
+            p["comments"],
+        )
+        old_data[int(id)] = post
+
+    return old_data
 
 def print_posts(posts: dict[Post]):
     for id, p in posts.items():
         print(str(id) + ": " + p.toString())
 
-def write_posts(posts: dict[Post]):
+def write_data(posts: dict[Post]):
     newFile = os.path.join(
         os.path.dirname(__file__), "..", "..", "public", "data.json"
     )
     new_path = Path(newFile)
-
-
     new_data = dict()
+
     for id, p in posts.items():
         new_data[id] = {"date": p.date, "image": p.image, "description": p.description, "tags": p.tags, "likes": p.likes, "comments": p.comments}
-
-    
     all_posts = json.dumps(new_data, indent=4)
+    # write final changes to data.json
     with open(new_path, 'w') as storage:
         storage.write(all_posts)
+
+    secondFile = os.path.join(
+        os.path.dirname(__file__), "..", "..", "public", "data.py"
+    )
+    data = Path(secondFile)
+    # write final changes to data.py
+    with open(data, "w") as f:
+        f.write(str(new_data))
     
 
 if __name__ == "__main__":
-    new_posts = read_post()
-    # print_posts(new_posts)
-    write_posts(new_posts)
+    new_changes = read_new()
+    # print_posts(new_changes)
+    old = get_old()
+    old.update(new_changes)
+    write_data(old)
     
 
     
