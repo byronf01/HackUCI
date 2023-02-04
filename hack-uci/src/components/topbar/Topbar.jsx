b4d739a3165aae5f2416bd3450ee43d5bdcc2092
Alex Figura <wfigura@uci.edu>
2023-02-04 11:28:15 -0800

    Image Slider

--- hack-uci/src/components/topbar/Topbar.jsx
+++ hack-uci/src/components/topbar/Topbar.jsx
@@ -1,48 +1,32 @@
 import "./topbar.css";
-import {PersonSearchOutlined, Person3Outlined, ChatBubbleOutline, Notifications} from "@mui/icons-material";
+import SearchIcon from '@mui/icons-material/Search';
+
 
 export default function Topbar() {
-    return(
-        <div className="topbarContainer">
-            <div className="topbarLeft">
-                <span className="logo">DeadlyHacks</span>
-            </div>
-            <div className="topbarCenter">
-                <div className="searchbar">
-                    <PersonSearchOutlined className="searchIcon"/>
-                    <input 
-                        placeholder="Search for club or post"
-                        className="searchInput"/>
-            </div>
-            <div>
-
-
-            </div>
-            <div className="topbarRight">
-                <div className="topbarLinks">
-                    <span className = "topbarLink">Clubs</span>
-                    <span className = "topbarLink">Friends</span>
-                </div>
-                <div className="topBarIcons">
-                    <div className="topbarIconItem">
-                        <Person3Outlined />
-                        <span className="topbarIconBadge">1</span>
-                    </div>
-                    <div className="topbarIconItem">
-                        <ChatBubbleOutline />
-                        <span className="topbarIconBadge">1</span>
-                    </div>
-                    <div className="topbarIconItem">
-                        <Notifications />
-                        <span className="topbarIconBadge">1</span>
-                    </div>
-                </div>
-            </div>
-
-            <img src="/Assets/Image1.png" alt="" className="topbarImg"/>    
-        </div>   
+    return (
+      <div className="topbarContainer">
+        <div className="topbarLeft">
+          <span className="logo">DeadlyHacks</span>
         </div>
-
-    )
-
-}
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <SearchIcon className="searchIcon" />
+            <input
+              placeholder="Search for Club Posting"
+              className="searchInput"
+            />
+          </div>
+        </div>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Clubs</span>
+            <span className="topbarLink">Friends</span>
+            <span className="topbarLink">Events</span>
+            <span className="topbarLink">Calander</span>
+          </div>
+        
+          </div>
+          <img src="/Assets/Image1.png" alt="" className="topbarImg"/>
+        </div>
+    );
+  }
