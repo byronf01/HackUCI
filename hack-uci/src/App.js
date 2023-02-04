92dfeda59933fe08c888afb16df689ae4b08cc9c
Ben Boben <bboben@uci.edu>
2023-02-04 15:46:14 -0800

    Routes and Links to home and calendar.

--- hack-uci/src/App.js
+++ hack-uci/src/App.js
@@ -1,16 +1,24 @@
-import Home from "./pages/home/Home"
-<<<<<<< HEAD
-import Club from "./pages/Clubs/Club"
-
-function App() {
-  return <Club/>
-=======
-import Calendar from "./pages/home/calendar/calendar";
-
-function App() {
-  return <Calendar/>
->>>>>>> 7a2a6460581718ebc78760038e321161391da164
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ReactDOM from "react-dom";
+import Home from "./pages/home/Home";
+import Calendar from './pages/home/calendar/calendar';
+import Error from './pages/home/Error';
+import Layout from './components/topbar/layout';
+ 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Calendar" element={<Calendar />} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App;
-
+ReactDOM.render(<App />, document.getElementById("root"));
--- hack-uci/src/components/topbar/Topbar.jsx
+++ hack-uci/src/components/topbar/Topbar.jsx
@@ -1,13 +1,17 @@
 import "./topbar.css";
 import SearchIcon from '@mui/icons-material/Search';
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+
 
 
 export default function Topbar() {
     return (
       <div className="topbarContainer">
-        <div className="topbarLeft">
-          <span className="logo">DeadlyHacks</span>
-        </div>
+        <ul className="topbarLeft">
+              <li>
+                <NavLink to="/" className="logo">DeadlyHacks</NavLink>
+              </li>
+          </ul>
         <div className="topbarCenter">
           <div className="searchbar">
             <SearchIcon className="searchIcon" />
@@ -17,15 +21,26 @@ export default function Topbar() {
             />
           </div>
         </div>
-        <div className="topbarRight">
-          <div className="topbarLinks">
-            <span className="topbarLink">Clubs</span>
-            <span className="topbarLink">Friends</span>
+        {/* <div className="topbarRight"> */}
+          {/* <div className="topbarLinks"> */}
+          <nav>
+            <ul className="topbarRight">
+            <li>
+                <NavLink to="/clubs" className="topbarLinks">Clubs</NavLink>
+              </li>
+              <li>
+                <NavLink to="/post" className="topbarLinks">Post</NavLink>
+              </li>
+              <li>
+                <NavLink to="/calendar" className="topbarLinks">Calendar</NavLink>
+              </li>
+            </ul>
+          </nav>
+            {/* <span className="topbarLink">Friends</span>
             <span className="topbarLink">Events</span>
-            <span className="topbarLink">Calander</span>
-          </div>
-        
-          </div>
+            <span className="topbarLink">Calander</span> */}
+          {/* </div> */}
+        {/* </div> */}
           <img src="/Assets/Image1.png" alt="" className="topbarImg"/>
         </div>
     );
