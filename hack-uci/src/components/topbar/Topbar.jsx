11e740a66ee1b20dd598bb4804f163edc6de94af
Alex Figura <wfigura@uci.edu>
2023-02-04 02:08:06 -0800

    Design Improvement

---
+++ hack-uci/src/components/topbar/Topbar.jsx
@@ -0,0 +1,44 @@
+import "./topbar.css";
+import {PersonSearchOutlined, Person3Outlined, ChatBubbleOutline, Notifications} from "@mui/icons-material";
+
+export default function Topbar() {
+    return(
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">DeadlyHacks</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <PersonSearchOutlined />
+                    <input 
+                        placeholder="Search for club or post"
+                        className="searchInput"/>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className = "topbarLink">Clubs</span>
+                    <span className = "topbarLink">Friends</span>
+                </div>
+                <div className="topBarIcons">
+                    <div className="topbarIconItem">
+                        <Person3Outlined />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <ChatBubbleOutline />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+            </div>
+
+            <img src="/Assets/Image1.png" alt="" className="topbarImg"/>    
+        </div>   
+        </div>
+
+    )
+
+}
---
+++ hack-uci/src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import Topbar from "../../components/topbar/Topbar";
+
+export default function Home(){
+    return(
+        <div>
+            <Topbar/>
+        </div>
+
+    )
+}
