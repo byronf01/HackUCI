3a72f40544ec535aa301900d08ff25c6428ccd19
Alex Figura <wfigura@uci.edu>
2023-02-04 16:25:56 -0800

    Update to Profile

--- hack-uci/src/components/rightbar/Rightbar.jsx
+++ hack-uci/src/components/rightbar/Rightbar.jsx
@@ -3,10 +3,22 @@ import "./rightbar.css";
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
-
+            <div className="rightbarWrapper">
+                <div className="trendingCategoriesContainer">
+                    <div className='Sports'>
+                        <span className="SportsText">
+                            Sports
+                        </span>
+                    </div>
+                    <div className="Academics">
+                        <span className="AcademicsText">Academics</span>
+                    </div>
+                    <div className="Music">
+                        <span className="MusicText">Music</span>
+                    </div>
+                </div>
+            </div>
         </div>
-
     )
 
 }
