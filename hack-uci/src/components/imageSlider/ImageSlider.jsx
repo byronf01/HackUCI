ca581d6e691955d319d9585945964b7045099a9e
Ben Boben <bboben@uci.edu>
2023-02-04 17:43:20 -0800

    Dynamic size picture slider for home page.

--- hack-uci/src/components/imageSlider/ImageSlider.jsx
+++ hack-uci/src/components/imageSlider/ImageSlider.jsx
@@ -1,14 +1,17 @@
 import "./ImageSlider.css";
 import SimpleImageSlider from "react-simple-image-slider";
-
+import useWindowDimensions from "./windowDimensions";
 const images = [ {url: "/Assets/Test1.jpg"} , {url: "/Assets/Test2.jpg"}, {url: "/Assets/Test3.jpg"}]
 
+
+
 export default function Slider() {
+    const { height, width } = useWindowDimensions();
     return (
         <div className="sliderContainer">
             <SimpleImageSlider        className="ImageSlider"
-                width={1600}
-                height={700}
+                width={width}
+                height={height}
                 images={images}
                 showBullets={true}
                 showNavs={true}
